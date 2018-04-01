c24c28e621a8c8fe725338201335cb2209ac36c6
happy3014 <lvhuanle24@gmail.com>
2018-04-01 10:16:27 +0800

    安卓端代码完成

--- app/src/main/java/com/lab/bluetoothflashbar/ModifyActivity.java
+++ app/src/main/java/com/lab/bluetoothflashbar/ModifyActivity.java
@@ -13,6 +13,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.yyb.AsciiZimoUtils;
+import com.yyb.Hzk16Uttils;
+
+import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 
 public class ModifyActivity extends AppCompatActivity {
@@ -20,9 +24,10 @@ public class ModifyActivity extends AppCompatActivity {
     private static final String TAG = "BLE_DEBUG";
 
     private EditText editTextInput;
-    private CheckBox checkBoxIsChinese;
+    private EditText editTextNum;
     private Button btnSend;
     private Button btnRead;
+    private Button btnDisconnect;
     private TextView tvDisplay;
 
 
@@ -36,46 +41,67 @@ public class ModifyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modify);
 
-        editTextInput = (EditText)findViewById(R.id.et_input);
-        checkBoxIsChinese = (CheckBox)findViewById(R.id.cb_is_chinese);
-        btnSend = (Button)findViewById(R.id.btn_send);
-        btnRead = (Button)findViewById(R.id.btn_read);
+        editTextInput = (EditText) findViewById(R.id.et_input);
+        editTextNum = (EditText) findViewById(R.id.et_num);
+        btnSend = (Button) findViewById(R.id.btn_send);
+        btnRead = (Button) findViewById(R.id.btn_read);
+        btnDisconnect = (Button) findViewById(R.id.btn_disconnect);
 
-        tvDisplay = (TextView)findViewById(R.id.tv_dis);
+        tvDisplay = (TextView) findViewById(R.id.tv_dis);
 
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String s = editTextInput.getText().toString();
+                String s = editTextNum.getText().toString();
                 boolean isF = "1".equals(s);
                 byte[] buffer = new byte[20];
-                if(isF){
+                if (isF) {
                     buffer[0] = 1;
 
-                }else{
+                } else {
                     buffer[0] = 2;
                 }
+                s = editTextInput.getText().toString();
+                if (s == null || s.trim().length() == 0 || s.trim().length() > 4) {
+                    Toast.makeText(ModifyActivity.this, "请输入长度为1-4的字符串", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                String[] ss = s.trim().split("");
+                byte[] buffers = new byte[128];
+                int index = 0;
+                for (int i=0; i<ss.length; i++) {
+                    String s1 = ss[i];
+                    byte[] bf1 = Hzk16Uttils.readSingle(s1);
+                    if (bf1 == null) {
+                        bf1 = AsciiZimoUtils.readSingleAsciiZimo(s1);
+                    }
+                    if (bf1 == null) {
+                        Toast.makeText(ModifyActivity.this, "请输入合法字符", Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                    System.arraycopy(bf1, 0, buffers, index, bf1.length);
+                    index+=bf1.length;
+                }
+                byte[] chas = new byte[8];
+                try {
+                    byte[] bb = s.trim().getBytes("GB2312");
+                    System.arraycopy(bb, 0, chas, 0, bb.length);
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
                 byte i = 0;
-                for(i=0; i>=0 && i<=112; i+=16){
+                for (i = 0; i >= 0 && i <= 112; i += 16) {
+                    int k = i/16;
                     buffer[1] = i;
-                    if(isF){
-                        buffer[2] = 0x00;
-                        buffer[3] = 98;
-                    }else{
-                        buffer[2] = 0x00;
-                        buffer[3] = 99;
+                    if(k<4) {
+                        buffer[2] = chas[2 * i];
+                        buffer[3] = chas[2 * i + 1];
                     }
-                    for(int j=4; j<=19; j++){
-                        if(isF) {
-                            buffer[j] = 0x0f;
-                        }else{
-                            buffer[j] = 0x0e;
-                        }
-                    }
-                    Log.i(TAG, "开始发送："+Arrays.toString(buffer));
+                    System.arraycopy(buffers, i, buffer, 4, 16);
+                    Log.i(TAG, "开始发送：" + Arrays.toString(buffer));
                     send(buffer);
                     try {
-                        Thread.sleep(3000);
+                        Thread.sleep(1000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -91,14 +117,14 @@ public class ModifyActivity extends AppCompatActivity {
         });
     }
 
-    private void send(byte[] bytes){
+    private void send(byte[] bytes) {
 
-        if(!GattCallbackImpl.discovered){
+        if (!GattCallbackImpl.discovered) {
             Toast.makeText(this, "未连接至蓝牙", Toast.LENGTH_LONG).show();
             return;
         }
 
-        if(gatt==null || device==null || characteristicWrite==null){
+        if (gatt == null || device == null || characteristicWrite == null) {
             gatt = Utils.gatt;
             device = Utils.currentDevice;
             characteristicWrite = Utils.characteristicWrite;
@@ -106,31 +132,31 @@ public class ModifyActivity extends AppCompatActivity {
         }
 
         characteristicWrite.setValue(bytes);
-        if(gatt.writeCharacteristic(characteristicWrite)){
+        if (gatt.writeCharacteristic(characteristicWrite)) {
             Log.i(TAG, "发送成功");
-        }else{
+        } else {
             Log.i(TAG, "发送失败");
         }
     }
 
-    private void read(){
-        if(!GattCallbackImpl.discovered){
+    private void read() {
+        if (!GattCallbackImpl.discovered) {
             Toast.makeText(this, "未连接至蓝牙", Toast.LENGTH_LONG).show();
             return;
         }
 
-        if(gatt==null || device==null || characteristicWrite==null){
+        if (gatt == null || device == null || characteristicWrite == null) {
             gatt = Utils.gatt;
             device = Utils.currentDevice;
             characteristicWrite = Utils.characteristicWrite;
             characteristicRead = Utils.characteristicRead;
         }
 
-        if(gatt.readCharacteristic(characteristicRead)){
+        if (gatt.readCharacteristic(characteristicRead)) {
             StringBuilder stringBuilder = new StringBuilder(tvDisplay.getText().toString());
-            stringBuilder.append(Arrays.toString(characteristicRead.getValue())+"\n");
+            stringBuilder.append(Arrays.toString(characteristicRead.getValue()) + "\n");
             tvDisplay.setText(stringBuilder.toString());
-        }else{
+        } else {
             Toast.makeText(this, "读取失败", Toast.LENGTH_LONG).show();
         }
     }
